22f1f11194ba897d262322b5f09d7f56bf72477b
KamGytis <gytisskaminskas@gmail.com>
2026-05-22 00:05:19 +0300

    added first word checker for future usage

--- app/controllers/terminal_controller.rb
+++ app/controllers/terminal_controller.rb
@@ -3,19 +3,21 @@ class TerminalController < ApplicationController
   end
 
   def execute
-    command = params[:command]
 
-    allowed = ["ls", "pwd", "whoami", "echo hello", "help", "system_info"]
+    raw_input = params[:command]
+    command = raw_input.split(" ") 
 
-    return render json: { output: "Command not allowed" } unless allowed.include?(command)
+    allowed = ["ls", "pwd", "whoami", "echo", "help"]
+
+    return render json: { output: "Command not allowed" } unless allowed.include?(command[0])
 
     output =
-      case command
+      case command[0]
       when "ls" then `ls`
       when "pwd" then `pwd`
       when "whoami" then `whoami`
-      when "echo hello" then "hello"
-      when "help" then "You can use commands: echo hello, whoami, pwd, ls"
+      when "echo" then "hello"
+      when "help" then "You can use commands: echo, whoami, pwd, ls"
       end
 
     render json: { output: output }
--- app/javascript/terminal.js
+++ app/javascript/terminal.js
@@ -1,37 +1,37 @@
 function initTerminal() {
-  const input = document.getElementById("input");
-  const output = document.getElementById("output");
+ const input = document.getElementById("input");
+ const output = document.getElementById("output");
 
-  if (!input || !output) return;
+ if (!input || !output) return;
 
-  console.log("terminal initialized");
+ console.log("terminal initialized");
 
-  function print(text) {
-    output.innerHTML += `<div>${text}</div>`;
-  }
+ function print(text) {
+ output.innerHTML += `<div>${text}</div>`;
+ }
 
-  input.addEventListener("keydown", async (e) => {
-    if (e.key !== "Enter") return;
+ input.addEventListener("keydown", async (e) => {
+ if (e.key !== "Enter") return;
 
-    const command = input.value.trim();
-    if (!command) return;
+ const command = input.value.trim();
+ if (!command) return;
 
-    print("> " + command);
+ print("> " + command);
 
-    const res = await fetch("/execute", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-CSRF-Token": document.querySelector('[name="csrf-token"]').content
-      },
-      body: JSON.stringify({ command })
-    });
+const res = await fetch("/execute", {
+ method: "POST",
+ headers: {
+ "Content-Type": "application/json",
+ "X-CSRF-Token": document.querySelector('[name="csrf-token"]').content
+ },
+ body: JSON.stringify({ command })
+ });
 
-    const data = await res.json();
-    print(data.output);
+ const data = await res.json();
+ print(data.output);
 
-    input.value = "";
-  });
+ input.value = "";
+});
 }
 
 document.addEventListener("turbo:load", initTerminal);
